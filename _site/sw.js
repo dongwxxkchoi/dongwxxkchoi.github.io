283627ea63d2d8c3e4c317ff917c7f4c77e45f5d
Dwgygygy <dwchoi0610@gmail.com>
2023-01-12 19:07:05 +0900

    how?

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230112.185845';
+const cacheName = 'chirpy-20230112.190651';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
