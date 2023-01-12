10c96ad199c079ed48b8564a47c2e969fdf71551
Dwgygygy <dwchoi0610@gmail.com>
2023-01-12 22:04:20 +0900

    change layouts

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230112.190651';
+const cacheName = 'chirpy-20230112.220353';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
